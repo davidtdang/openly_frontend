15eb7a5479ad660d33520b81332102ba183b3af0
David Dang <dtudang@gmail.com>
2015-03-30 22:39:16 -0700

    added navbar, search bar, map, some navbar styling

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+// var source = $("#search-results-template").html();
+// var searchResultsTemplate = Handlebars.compile(source);
+// var featureLayers = [];
+
+// Provide your access token
+L.mapbox.accessToken = 'pk.eyJ1IjoiZGF2aWRkYW5nIiwiYSI6Ikh0OXhteVUifQ.Ub2aDG7dWw0Hu3ytuDEg-g';
+// Create a map in the div #map
+L.mapbox.map('map', 'daviddang.ljfi6cpf').setView([37.767, -122.436], 13);
+
+
+// $("#search-results").on("click", ".venue", function(){
+//
+//
+//   var venue = {
+//     name: name,
+//     address: address,
+//     phone: phone
+//   }
+// })
