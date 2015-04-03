4428dd9978c9e4112f844cd73b01710811a2c627
David Dang <dtudang@gmail.com>
2015-04-02 23:42:24 -0700

    revert back to using yelp api stable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,15 +9,14 @@ map = L.mapbox.map('map', 'daviddang.ljfi6cpf').setView([37.767, -122.436], 13);
 
 
 $("#search-results").on("click", ".venue", function() {
-  var divCoords = $(this).data("coordinates").split(",");
-  var geocoords = [parseFloat(divCoords[0]), parseFloat(divCoords[1])]
+  var divCoords = $(this).data("coordinates").split(',');
   var name = $(this).data("name");
   var address = $(this).data("address");
   var phone = $(this).data("phone");
-  // var rating = $(this).data("rating");
+  var rating = $(this).data("rating");
 
   var venue = {
-    coords: geocoords,
+    coords: [parseFloat(divCoords[0]), parseFloat(divCoords[1])],
     name: name,
     address: address,
     phone: phone
@@ -33,7 +32,7 @@ $("#search-form").on("submit", function (e) {
 
 function searchAction(searchTerm) {
   var query = searchTerm.replace(/\s/g, "+");
-  var url = "http://localhost:3000/search";
+  var url = "http://localhost:3000/search_yelp";
   var req = {
     url: url,
     method: "post",
@@ -45,8 +44,10 @@ function searchAction(searchTerm) {
   $.ajax(req)
     .done(function (res) {
       // append to list
-      console.log(res);
       setSearchResultsTemplate({venues:res});
+      res.forEach(function(venue){
+        addMarker(venue);
+      });
     })
     .fail(function () {
       throw "Search AJAX Failed";
@@ -74,7 +75,8 @@ function addMarker(venue) {
         type: 'Point',
         // coordinates here are in longitude, latitude order because
         // x, y is the standard for GeoJSON and many formats
-        coordinates: venue.coords
+        // coordinates: venue.coords
+        coordinates: [venue.geocoords[0], venue.geocoords[1]]
     },
     properties: {
         title: venue.name,
@@ -91,3 +93,103 @@ function addMarker(venue) {
   featureLayer.addTo(map);
   featureLayers.push(featureLayer);
 }
+
+$("#search-results").on("click", ".category", function () {
+  var category = $(this).text().trim();
+  $("#search-form input").val(category);
+  searchAction(category);
+});
+
+// var source   = $("#search-results-template").html();
+// var searchResultsTemplate = Handlebars.compile(source);
+// var featureLayers = [];
+//
+// // Provide your access token
+// L.mapbox.accessToken = 'pk.eyJ1IjoiZGF2aWRkYW5nIiwiYSI6Ikh0OXhteVUifQ.Ub2aDG7dWw0Hu3ytuDEg-g';
+// // Create a map in the div #map
+// map = L.mapbox.map('map', 'daviddang.ljfi6cpf').setView([37.767, -122.436], 13);
+//
+//
+// $("#search-results").on("click", ".venue", function() {
+//   var divCoords = $(this).data("coordinates").split(",");
+//   var geocoords = [parseFloat(divCoords[0]), parseFloat(divCoords[1])]
+//   var name = $(this).data("name");
+//   var address = $(this).data("address");
+//   var phone = $(this).data("phone");
+//   // var rating = $(this).data("rating");
+//
+//   var venue = {
+//     coords: geocoords,
+//     name: name,
+//     address: address,
+//     phone: phone
+//   };
+//
+//   addMarker(venue);
+// });
+//
+// $("#search-form").on("submit", function (e) {
+//   e.preventDefault();
+//   searchAction($("#search-form input").val());
+// });
+//
+// function searchAction(searchTerm) {
+//   var query = searchTerm.replace(/\s/g, "+");
+//   var url = "http://localhost:3000/search";
+//   var req = {
+//     url: url,
+//     method: "post",
+//     data:{
+//       "s":query
+//     }
+//   };
+//
+//   $.ajax(req)
+//     .done(function (res) {
+//       // append to list
+//       console.log(res);
+//       setSearchResultsTemplate({venues:res});
+//     })
+//     .fail(function () {
+//       throw "Search AJAX Failed";
+//     });
+// }
+//
+// function setSearchResultsTemplate(context) {
+//   var html = searchResultsTemplate(context);
+//   $("#search-results").html(html);
+//   $('.ui.accordion').accordion("close others");
+//   featureLayers.forEach(function (featureLayer) {
+//     map.removeLayer(featureLayer);
+//   });
+//   featureLayers = [];
+// }
+//
+//
+//
+// function addMarker(venue) {
+//   var featureLayer = L.mapbox.featureLayer({
+//     // this feature is in the GeoJSON format: see geojson.org
+//     // for the full specification
+//     type: 'Feature',
+//     geometry: {
+//         type: 'Point',
+//         // coordinates here are in longitude, latitude order because
+//         // x, y is the standard for GeoJSON and many formats
+//         coordinates: venue.coords
+//     },
+//     properties: {
+//         title: venue.name,
+//         description: "<div>"+ venue.address +"</div>" + "<div>"+ venue.phone +"</div>",
+//         // description: venue.address,
+//         // one can customize markers by adding simplestyle properties
+//         // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+//         'marker-size': 'large',
+//         'marker-color': '#FC3A57',
+//         // 'marker-symbol': 'cafe'
+//
+//     }
+//   });
+//   featureLayer.addTo(map);
+//   featureLayers.push(featureLayer);
+// }
