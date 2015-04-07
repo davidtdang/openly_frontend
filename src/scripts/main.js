a3d7d4ebf0572a775a2ef869efff3141321fbc49
David Dang <dtudang@gmail.com>
2015-04-07 16:09:26 -0700

    able to set user tips (front end)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,26 @@
+
+
+
+$('#search-results').on("click", ".user_tip_modal_popup_button",function(){
+  var $venue = $(this).closest(".venue");
+
+  var id = $venue.data("id");
+  var tips = $venue.data("userTips");
+
+  $("#tipModal").data("id", id);
+  $("#tipModal").data("tips", tips);
+  console.log($("#tipModal").data());
+
+  $('.ui.modal').modal('show');
+});
+
+
+
+
 var source   = $("#search-results-template").html();
 var searchResultsTemplate = Handlebars.compile(source);
+// var source_two   = $("#user-tips-template").html();
+// var tipsTemplate = Handlebars.compile(source_two);
 var featureLayers = [];
 
 // Provide your access token
@@ -14,16 +35,29 @@ $("#search-results").on("click", ".venue", function() {
   var address = $(this).data("address");
   var phone = $(this).data("phone");
   var rating = $(this).data("rating");
-  var hours = $(this).data("hours");
+  var opening = $(this).data("opening");
+  var closing = $(this).data("closing");
+  var hours_left = $(this).data("hours_left");
+  console.log(">>", $(this).data());
+  // var userTip = $('active.title .user-tip-indicator').text();
+
 
   var venue = {
     coords: [parseFloat(divCoords[0]), parseFloat(divCoords[1])],
     name: name,
     address: address,
-    phone: phone
+    phone: phone,
+    opening: opening,
+    closing: closing
   };
 
   addMarker(venue);
+});
+
+$("#search-results").on("click", ".category", function () {
+  var category = $(this).text().trim();
+  $("#search-form input").val(category);
+  searchAction(category);
 });
 
 $("#search-form").on("submit", function (e) {
@@ -31,10 +65,24 @@ $("#search-form").on("submit", function (e) {
   searchAction($("#search-form input").val());
 });
 
+//////// CREATE TRIGGERING ACTION METHOD TO SUBMIT TIP
+$("#tipModal").on("submit", " form", function (e) {
+  e.preventDefault();
+  // var userTip = $(".content.active .userTipForm input").val();
+  // if (userTip === "") {
+  //   return;
+  // }
+  var id = $("#tipModal").data("id");
+  var text = $("#tipModal :input").val();
+  console.log(id);
+  addUserTipAction(text, id);
+  // removeUserTipForm($(this));
+});
+
 
 function searchAction(searchTerm) {
   var query = searchTerm.replace(/\s/g, "+");
-  var url = "http://localhost:3000/search_yelp";
+  var url = "http://localhost:3000/find_venues";
   var req = {
     url: url,
     method: "post",
@@ -46,10 +94,12 @@ function searchAction(searchTerm) {
   $.ajax(req)
     .done(function (res) {
       // append to list
+      // console.log(res)
       setSearchResultsTemplate({venues:res});
       res.forEach(function(venue){
         addMarker(venue);
       });
+      setSearchResultsData();
     })
     .fail(function () {
       throw "Search AJAX Failed";
@@ -66,9 +116,21 @@ function setSearchResultsTemplate(context) {
   featureLayers = [];
 }
 
-
+function setSearchResultsData() {
+  $(".venue[data-db='true']").each(function (i, el) {
+    var $el = $(el);
+    var id = $el.data("id");
+    $.ajax({
+      url: "http://localhost:3000/user_tips/"+id,
+      method: 'get'
+    }).done(function (res) {
+      $el.data("userTips", res)
+    })
+  })
+}
 
 function addMarker(venue) {
+  // console.log(venue)
   var featureLayer = L.mapbox.featureLayer({
     // this feature is in the GeoJSON format: see geojson.org
     // for the full specification
@@ -96,15 +158,64 @@ function addMarker(venue) {
   featureLayers.push(featureLayer);
 }
 
-$("#search-results").on("click", ".category", function () {
-  var category = $(this).text().trim();
-  $("#search-form input").val(category);
-  searchAction(category);
-});
 
-$('#example2').progress({
-  percent: 22
-});
+
+// $('#example2').progress({
+//   percent: 22
+// });
+
+
+function addUserTipAction(userTip, id){
+  console.log(id)
+  var payload = {
+    user_tip:userTip,
+    venue_id:id
+  };
+
+  var url = "http://localhost:3000/user_tips";
+  // var url = "http://openly.herokuapp.com";
+
+  var req ={
+    url: url,
+    method: "post",
+    data:payload
+  };
+
+  $.ajax(req)
+    .done(function(res){
+      $('ul').append("<li>" + userTip + "</li>")
+      // setActiveVenue(userTip);
+    })
+    .fail(function(){
+      throw "Search AJAX Failed"
+    });
+
+}
+
+function removeUserTipForm($form) {
+  $form.fadeOut("fast", function () {
+    $form.remove();
+  });
+}
+
+// function setActiveVenue(userTip) {
+//   $('.active.title .user-tip-indicator').html(userTip + '<i class="wait icon"></i>');
+// }
+
+
+
+
+
+
+
+
+// $('.ui.label.tip').on('click', function(){
+//   $('.ui.modal').modal('show');
+// });
+
+
+
+
 
 
 //
